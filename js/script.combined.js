641cee7762afcb2f83e1958cd060c3a018348835
Cerdic <cedric@yterium.com>
2010-10-27 15:48:08 +0200

    combiner css et js via htaccess

---
+++ js/script.combined.js
@@ -0,0 +1,7 @@
+<!--#include file="jquery.js" -->
+<!--#include file="f_market.js" -->
+<!--#include file="package.js" -->
+<!--#include file="master.js" -->
+<!--#include file="jquery.autocomplete.js" -->
+<!--#include file="jquery.ajaxQueue.js" -->
+<!--#include file="f_body.js" -->
