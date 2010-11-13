3b9c88252d2131a1488be3576512ef77cc8447f9
Cerdic <cedric@yterium.com>
2010-11-13 14:21:04 +0100

    variante lazyload agressif : - seul le top et le contenu sont envoyes directement, les colonnes laterales et le pied sont charges en asynchrone par le js - seul jQuery est charge, le reste des script est charge en async par $.getScript - le src des images n'est pas reference dans le html, on utilise lazyload pour charger les images visibles en asynch - pour grapiller des centiemes de seconde, on utilise des img.preload en debut de <body> pour charger tout de suite les 3 plus grosses
    Pour preserver l'accessibilite aux utilisateur sans js, on place un meta refresh dans une balise <noscript> en debut de <body>. C'est non-valide mais fonctionnel. Dans le monde reel, on en profiterait pour poser un cookie 'nojs' et envoyer le site sans lazyload par la suite pour ces utilisateurs.
    
    Tout cela permet d'arriver a un onload de 0.998s sur le meilleur tir.
    http://www.webpagetest.org/result/101113_9674dde8f478062f0167d38becf8eee0/

---
+++ js/script.ll.combined.js
@@ -0,0 +1,5 @@
+<!--#include file="jquery.lazyload.js" -->
+<!--#include file="f_market.js" -->
+<!--#include file="package.js" -->
+<!--#include file="master.js" -->
+<!--#include file="f_body.js" -->
